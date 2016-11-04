def6606f0aa37cf2b0e51de11003f94fd00aa167
Dustin <dustinnewman98@gmail.com>
2016-11-03 20:46:00 -0700

    double quotes & classes

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 jQuery(document).ready(function(){
-        jQuery('#national-button').on('click', function(event) {        
-             jQuery('#national').toggle('show');
+        jQuery(".national-button").on("click", function(event) {        
+             jQuery(".national").toggle('show');
         });
     });
 
 jQuery(document).ready(function(){
-        jQuery('#state-button').on('click', function(event) {        
-             jQuery('#state').toggle('show');
+        jQuery(".state-button").on("click", function(event) {        
+             jQuery(".state").toggle('show');
         });
     });
 
 jQuery(document).ready(function(){
-        jQuery('#local-button').on('click', function(event) {        
-             jQuery('#local').toggle('show');
+        jQuery(".local-button").on("click", function(event) {        
+             jQuery(".local").toggle("show");
         });
     });
     
