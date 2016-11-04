0e9e43e311f5a38ed94ce5d3a82c128c60068a0c
Howard Huang <howardhuang96@gmail.com>
2016-11-04 01:11:28 -0700

    fixed main.js layout, was showing wrong filters due to toggle logic error

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
-jQuery(document).ready(function () {
-    jQuery(".national-button").on("click", function (event) {
-        jQuery(".national").show();
-        jQuery(".state").toggle('hide');
-        jQuery(".local").toggle('hide');
+function switchFilters(on, off1, off2){
+  $(on).show();
+  $(on + "-button").addClass("selected");
+  if ($(off1 + "-button").hasClass("selected")){
+    $(off1).toggle('hide');
+    $(off1 + "-button").removeClass("selected");
+  }
+  if ($(off2 + "-button").hasClass("selected")){
+    $(off2).toggle('hide');
+    $(off2 + "-button").removeClass("selected");
+  }
+}
+
+$(document).ready(function () {
+    $(".national-button").on("click", function (event) {
+        switchFilters(".national", ".state", ".local");
     });
 
-    jQuery(".state-button").on("click", function (event) {
-        jQuery(".state").show();
-        jQuery(".national").toggle('hide');
-        jQuery(".local").toggle('hide');
+    $(".state-button").on("click", function (event) {
+        switchFilters(".state", ".national", ".local");
     });
 
-    jQuery(".local-button").on("click", function (event) {
-        jQuery(".local").show();
-        jQuery(".state").toggle('hide');
-        jQuery(".national").toggle('hide');
+    $(".local-button").on("click", function (event) {
+        switchFilters(".local", ".national", ".state");
+    });
+
+    $(".all").on("click", function(){
+      if (!$(".national-button").hasClass("selected")){
+        $(".national-button").addClass("selected");
+        $(".national").toggle('hide');
+      }
+      if (!$(".state-button").hasClass("selected")){
+        $(".state-button").addClass("selected");
+        $(".state").toggle('hide');
+      }
+      if (!$(".local-button").hasClass("selected")){
+        $(".local-button").addClass("selected");
+        $(".local").toggle('hide');
+      }
     });
 });
