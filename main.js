95b341f7cf0720a89ede60ae3c2216189694925c
Kareem Nosseir <knosseir@users.noreply.github.com>
2016-11-03 21:57:21 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-jQuery(document).ready(function(){
-        jQuery(".national-button").on("click", function(event) {        
-             jQuery(".national").toggle('show');
-        });
+jQuery(document).ready(function () {
+    jQuery(".national-button").on("click", function (event) {
+        jQuery(".national").show();
+        jQuery(".state").toggle('hide');
+        jQuery(".local").toggle('hide');
     });
+});
 
-jQuery(document).ready(function(){
-        jQuery(".state-button").on("click", function(event) {        
-             jQuery(".state").toggle('show');
-        });
+jQuery(document).ready(function () {
+    jQuery(".state-button").on("click", function (event) {
+        jQuery(".state").show();
+        jQuery(".national").toggle('hide');
+        jQuery(".local").toggle('hide');
     });
+});
 
-jQuery(document).ready(function(){
-        jQuery(".local-button").on("click", function(event) {        
-             jQuery(".local").toggle("show");
-        });
+jQuery(document).ready(function () {
+    jQuery(".local-button").on("click", function (event) {
+        jQuery(".local").show();
+        jQuery(".state").toggle('hide');
+        jQuery(".national").toggle('hide');
     });
-    
+});
