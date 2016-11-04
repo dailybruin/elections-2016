ecaa5a45baf984fd9bf980493d8d080078653ed2
Paulina Lei <paulina.y.lei@gmail.com>
2016-11-03 19:10:47 -0700

    all files

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("navMenu").innerHTML =
+'<nav class="top-bar" data-topbar role="navigation"><section class="top-bar-section">' +
+'<ul class="right">' +
+'<li role="presentation"><a href="endorsements.html">Endorsements</a></li>' +
+'<li role="presentation"><a href="opinion.html">Opinion</a></li>' +
+'<li role="presentation"><a href="news.html">News</a></li>' +
+'<li role="presentation"><a href="multimedia.html">Multimedia</a></li>' +
+'</ul></section></nav>';
