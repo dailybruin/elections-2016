da7391135f007d1ee3431baa64941225c3cd6348
anisa-bashiri <abashiri@ucla.edu>
2016-11-03 19:18:00 -0700

    added main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+        jQuery('#national-button').on('click', function(event) {        
+             jQuery('#article_1').toggle('show');
+        });
+    });
+
+jQuery(document).ready(function(){
+        jQuery('#state-button').on('click', function(event) {        
+             jQuery('#article_1').toggle('show');
+        });
+    });
+
+jQuery(document).ready(function(){
+        jQuery('#local-button').on('click', function(event) {        
+             jQuery('#article_1').toggle('show');
+        });
+    });
+    
