000d1c4dff63bed27f9bfffb8732fab3b43685b8
Paulina Lei <paulina.y.lei@gmail.com>
2016-11-04 01:31:10 -0700

    ay the nav bar got fixed B)

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.getElementById("header-img").innerHTML =
 document.getElementById("navMenu").innerHTML =
 '<nav class="redone-top-bar" data-topbar role="navigation"><section class="top-bar-section">' +
 '<ul class="right">' +
-'<li role="presentation"><a href="index.html">Endorsements</a></li>' +
-'<li role="presentation"><a href="opinion.html">Opinion</a></li>' +
-'<li role="presentation"><a href="news.html">News</a></li>' +
+'<li role="presentation"><a href="index.html">Endorsements</a></li>' + ' | ' +
+'<li role="presentation"><a href="opinion.html">Opinion</a></li>' + ' | ' +
+'<li role="presentation"><a href="news.html">News</a></li>' + ' | ' +
 '<li role="presentation"><a href="multimedia.html">Multimedia</a></li>' +
 '</ul></section></nav>';
 
